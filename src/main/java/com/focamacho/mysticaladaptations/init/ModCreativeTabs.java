823998a9f4dc1bef40ee5e8d5999f28a4311b697
Thomas-Gallagher <94071419+Thomas-Gallagher@users.noreply.github.com>
2025-08-13 19:26:40 +0100

    Update ModCreativeTabs.java

--- src/main/java/com/focamacho/mysticaladaptations/init/ModCreativeTabs.java
+++ src/main/java/com/focamacho/mysticaladaptations/init/ModCreativeTabs.java
@@ -1,42 +1,33 @@
 package com.focamacho.mysticaladaptations.init;
 
-import com.blakebr0.cucumber.util.FeatureFlagDisplayItemGenerator;
-import com.focamacho.mysticaladaptations.MysticalAdaptations;
-import com.focamacho.mysticaladaptations.config.ConfigHolder;
 import com.focamacho.mysticaladaptations.util.Reference;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.registries.RegisterEvent;
 
 public class ModCreativeTabs {
-    public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Reference.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> CREATIVE_TAB = REGISTRY.register("creative_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.mysticaladaptations"))
-            .icon(() -> new ItemStack(ModItems.INSANIUM_SWORD.get()))
-            .displayItems(FeatureFlagDisplayItemGenerator.create((parameters, output) -> {
-                output.accept(new ItemStack(ModAugments.HUNGERLESS.getItem()));
-                output.accept(new ItemStack(ModAugments.WOODCUTTER.getItem()));
-                output.accept(new ItemStack(ModAugments.MINING_AOE_V.getItem()));
-                output.accept(new ItemStack(ModAugments.STRENGTH_IV.getItem()));
-                output.accept(new ItemStack(ModAugments.ABSORPTION_VI.getItem()));
-                output.accept(new ItemStack(ModAugments.HEALTH_BOOST_VI.getItem()));
-                output.accept(new ItemStack(ModAugments.ATTACK_AOE_IV.getItem()));
-                output.accept(new ItemStack(ModAugments.TILLING_AOE_V.getItem()));
+    public static CreativeModeTab MYSTICAL_TAB;
 
-                if(ModList.get().isLoaded("vampirism")) {
-                    if(ConfigHolder.thirstlessAugment) output.accept(new ItemStack(ModAugments.THIRSTLESS.getItem()));
-                    if(ConfigHolder.daywalkerAugment) output.accept(new ItemStack(ModAugments.DAYWALKER.getItem()));
-                }
+    @SubscribeEvent
+    public static void registerTabs(RegisterEvent event) {
+        MYSTICAL_TAB = CreativeModeTab.builder()
+                .title(Component.translatable("itemGroup.mysticaladaptations"))
+                .icon(() -> new ItemStack(ModItems.INSANIUM_ESSENCE.get()))
+                .displayItems((params, output) -> {
+                    output.accept(ModItems.INSANIUM_ESSENCE.get());
+                    // Add more items/blocks as needed
+                })
+                .build();
 
-                for (RegistryObject<Item> entry : ModItems.items.getEntries()) {
-                    output.accept(entry);
-                }
-            }))
-            .build());
+        event.register(
+                Registries.CREATIVE_MODE_TAB,
+                ResourceLocation.fromNamespaceAndPath(Reference.MOD_ID, "mystical_tab"),
+                () -> MYSTICAL_TAB
+        );
+    }
 }
