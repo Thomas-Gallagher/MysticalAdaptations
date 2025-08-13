81969b873b54d9a6fb413c39279989aebfb39c26
Thomas-Gallagher <94071419+Thomas-Gallagher@users.noreply.github.com>
2025-08-14 00:04:42 +0100

    Update MobDropsHandler.java

--- src/main/java/com/focamacho/mysticaladaptations/handlers/MobDropsHandler.java
+++ src/main/java/com/focamacho/mysticaladaptations/handlers/MobDropsHandler.java
@@ -14,8 +14,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.event.entity.living.LivingDropsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
+import net.neoforged.bus.api.SubscribeEvent;
 
 import java.util.Collection;
 
