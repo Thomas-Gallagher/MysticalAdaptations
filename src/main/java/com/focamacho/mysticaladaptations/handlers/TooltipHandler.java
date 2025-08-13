2d80c0f03b25b9af366393ef2fe6df94df6f8c99
Thomas-Gallagher <94071419+Thomas-Gallagher@users.noreply.github.com>
2025-08-14 00:05:01 +0100

    Update TooltipHandler.java

--- src/main/java/com/focamacho/mysticaladaptations/handlers/TooltipHandler.java
+++ src/main/java/com/focamacho/mysticaladaptations/handlers/TooltipHandler.java
@@ -7,28 +7,29 @@ import com.focamacho.mysticaladaptations.util.Utils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.minecraft.core.registries.BuiltInRegistries;
 
 public class TooltipHandler {
 
     //Replace Tier 6 tooltips with a colored one
     @SubscribeEvent
-    public void tooltipEvent(ItemTooltipEvent event){
-        ResourceLocation rl = ForgeRegistries.ITEMS.getKey(event.getItemStack().getItem());
-        if(rl == null) return;
+    public void tooltipEvent(ItemTooltipEvent event) {
+        ResourceLocation rl = BuiltInRegistries.ITEM.getKey(event.getItemStack().getItem());
         String modid = rl.getNamespace();
 
-        if(modid.equals(Reference.MOD_ID) || modid.equals(MysticalAgriculture.MOD_ID)) {
+        if (modid.equals(Reference.MOD_ID) || modid.equals(MysticalAgriculture.MOD_ID)) {
             int index = -1;
-            for(Component text : event.getToolTip()){
-                if(text.equals(ModTooltips.getTooltipForTier(6))){
+            for (Component text : event.getToolTip()) {
+                if (text.equals(ModTooltips.getTooltipForTier(6))) {
                     index = event.getToolTip().indexOf(text);
                 }
             }
-            if(index != -1){
-                event.getToolTip().set(index, Component.literal(ChatFormatting.GRAY + String.format(Component.translatable("tooltip.mysticalagriculture.tier").getString(), Utils.getColorFromTier(6) + "6")));
+            if (index != -1) {
+                event.getToolTip().set(index, Component.literal(ChatFormatting.GRAY +
+                        String.format(Component.translatable("tooltip.mysticalagriculture.tier").getString(),
+                                Utils.getColorFromTier(6) + "6")));
             }
         }
     }
